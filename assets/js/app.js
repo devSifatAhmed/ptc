d85c1948349a7ba1254448cd72df09f46752468f
programmer-sifat-ahmed <programmer.sifatahmed@gmail.com>
2024-02-09 06:00:35 +0600

    landing page completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,3 +4,38 @@ $(document).ready(function(){
         time: 2500
     });
 });
+
+$('.slider-payment-methods').slick({
+    infinite: true,
+    speed: 2000,
+    slidesToShow: 5,
+    slidesToScroll: 4,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    dots: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+      
